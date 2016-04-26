fb3877c5c3fcf0837d709fe8ba7d9eeb4b6ab879
Anthony Gold <anthgold@gmail.com>
2016-04-26 16:54:04 -0700

    LeapYear.java needs more editing; adds templates index.vtl and detector.vtl

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,7 +1,20 @@
-public class LeapYear {
-  public static void main(String[] args) {}
+// This section will move to App.java later
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+// --- end of section ---
 
-  public boolean isLeapYear(Integer year) {
+public class LeapYear {
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+
+
+
+
+  public static boolean isLeapYear(Integer year) {
     if (year % 400 == 0) {
       return true;
     } else if (year % 100 == 0) {
