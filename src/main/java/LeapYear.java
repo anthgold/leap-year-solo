ff5989d65ed992a2027f9055a23ed54a84eeb1e4
Anthony Gold <anthgold@gmail.com>
2016-04-26 15:32:08 -0700

    sets up initial LeapYear.java and LeapYearTest.java; changes AppTest.java to AppTest.old to get past compiler error

---
+++ src/main/java/LeapYear.java
@@ -0,0 +1,8 @@
+public class LeapYear {
+  public static void main(String[] args) {}
+
+    public Boolean isLeapYear(Integer year) {
+      // code will go here
+      return false;
+  }
+}
---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class LeapYearTest {
+
+  @Test // nameOfFunction_testCase_expectedResult()
+  public void isLeapYear_forNumberDivisibleByFour_true() {
+    // testing code will go here
+  }
+}
