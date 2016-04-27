07c6339ba6db104e4a743d989d6b618347be894f
Anthony Gold <anthgold@gmail.com>
2016-04-26 20:46:57 -0700

    adds our Spark code to LeapYear.java

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -10,9 +10,25 @@ public class LeapYear {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine() );
 
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
 
+      String year = request.queryParams("year");
+      Integer integerYear = Integer.parseInt(year);
+      Boolean isLeapYear = isLeapYear(integerYear);
 
+      model.put("isLeapYear", isLeapYear);
+      model.put("year", request.queryParams("year"));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine() );
+  }
 
   public static boolean isLeapYear(Integer year) {
     if (year % 400 == 0) {
